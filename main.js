a0aabf68764296ec0f950cf8573d85ee5c9b94cb
WojtekTB <34536619+WojtekTB@users.noreply.github.com>
2019-03-26 01:02:50 -0700

    added some basic structure

---
+++ main.js
@@ -0,0 +1,16 @@
+var img;
+function preload(){
+  img = loadImage('dino.jpg'); // Load the image
+}
+
+function setup() {
+  createCanvas(720, 400);
+  const wchar_t kDisableWebSecurity[] = L"disable-web-security";
+}
+
+function draw() {
+  // Displays the image at its actual size at point (0,0)
+  image(img, 0, 0);
+  // Displays the image at point (0, height/2) at half size
+  image(img, 0, height / 2, img.width / 2, img.height / 2);
+}
