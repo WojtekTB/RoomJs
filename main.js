1a44ab4119ead67311576da66dc90a2f232a1a1e
WojtekTB <34536619+WojtekTB@users.noreply.github.com>
2019-03-26 03:14:15 -0700

    added a class for room
    basically just trying to populate the room with colors

--- main.js
+++ main.js
@@ -1,16 +1,49 @@
+var screenX = window.innerWidth;
+var screenY = window.innerHeight;
+var room;
+
 var img;
 function preload(){
-  img = loadImage('dino.jpg'); // Load the image
+  img = loadImage('dino.png'); // Load the image
 }
 
 function setup() {
-  createCanvas(720, 400);
-  const wchar_t kDisableWebSecurity[] = L"disable-web-security";
+  createCanvas(screenX, screenY);
+  background(100);
+  room = new Room(`rgb(100, 100, 100)`, `rgb(0, 0, 0)`);
 }
 
 function draw() {
-  // Displays the image at its actual size at point (0,0)
-  image(img, 0, 0);
-  // Displays the image at point (0, height/2) at half size
-  image(img, 0, height / 2, img.width / 2, img.height / 2);
+  // image(img, 0, 0);
+  room.show();
+
+}
+
+
+class Room{
+  constructor(wallColor, floorColor){
+    this.wallColor = wallColor;
+    this.floorColor = floorColor;
+    this.context = canvas.getContext('2d');
+  }
+
+  drawFloor(){
+    this.context.fillStyle = this.floorColor;
+    rect(0, window.innerHeight*(2/3), window.innerWidth, window.innerHeight/3);
+  }
+
+  drawWall(){
+    this.context.fillStyle = this.wallColor;
+    rect(0, 0, window.innerWidth, window.innerHeight*(2/3));
+  }
+
+  drawMisc(){
+
+  }
+
+  show(){
+    this.drawWall();
+    this.drawFloor();
+    this.drawMisc();
+  }
 }
